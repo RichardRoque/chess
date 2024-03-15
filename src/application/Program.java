0967c7499bdbed061d53077b70a55ae9fd39e155
Richard Roque <richard.roque@castgroup.com.br>
2024-03-15 12:57:51 -0300

    BoardException and defensive programmin

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,10 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
-		
+
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -3,7 +3,7 @@ package application;
 import chess.ChessPiece;
 
 public class UI {
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0; i<pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -14,7 +14,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
